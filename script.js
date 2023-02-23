bae0167fcdc43ac2015af0d322ccc6bc92ba446b
Jean-Charles <jeancharle.cano@outlook.fr>
2023-02-23 17:18:35 +0100

    update

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-let affiche = false; // variable pour indiquer si l'input est affiché
+// let affiche = false; // variable pour indiquer si l'input est affiché
 
-function afficher() {
-  if (affiche) {
-    document.getElementById("monLabel").style.display = "none";
-    document.getElementById("monInput").style.display = "none";
-    affiche = false;
-  } else {
-    document.getElementById("monLabel").innerHTML = "Nom :";
-    document.getElementById("monLabel").style.display = "inline";
-    document.getElementById("monInput").style.display = "inline";
-    affiche = true;
-  }
-}
+// function afficher() {
+//   if (affiche) {
+//     document.getElementById("monLabel").style.display = "none";
+//     document.getElementById("monInput").style.display = "none";
+//     affiche = false;
+//   } else {
+//     document.getElementById("monLabel").innerHTML = "Nom :";
+//     document.getElementById("monLabel").style.display = "inline";
+//     document.getElementById("monInput").style.display = "inline";
+//     affiche = true;
+//   }
+// }
