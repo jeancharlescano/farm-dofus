62fc9706fd3b23120e71adf0283c1168c6a2281a
Jean-Charles <jeancharle.cano@outlook.fr>
2023-02-23 16:42:14 +0100

    grid list

---
+++ script.js
@@ -0,0 +1,14 @@
+let affiche = false; // variable pour indiquer si l'input est affiché
+
+function afficher() {
+  if (affiche) {
+    document.getElementById("monLabel").style.display = "none";
+    document.getElementById("monInput").style.display = "none";
+    affiche = false;
+  } else {
+    document.getElementById("monLabel").innerHTML = "Nom :";
+    document.getElementById("monLabel").style.display = "inline";
+    document.getElementById("monInput").style.display = "inline";
+    affiche = true;
+  }
+}
