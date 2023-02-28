f2a1097127baaef3209b0cb9eae032485fa2fe37
Jean-Charles <jeancharles.cano@outlook.fr>
2023-02-28 19:19:44 +0100

    houblon

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+function calculateRessources(ressource) {
+  if (ressource == "orge") {
+    let nb = document.getElementById("orgeInput").value;
+    let res = nb / 4;
+    document.getElementById("ortie").innerHTML = `= ${res}`;
+  } else if (ressource == "avoine") {
+    let nb = document.getElementById("avoineInput").value;
+    let res = nb / 5;
+    document.getElementById("aubergine").innerHTML = `= ${res}`;
+    document.getElementById("sauge").innerHTML = `= ${res}`;
+  } else if (ressource == "houblon") {
+    let nb = document.getElementById("houblonInput").value;
+    let res = nb / 5;
+    document.getElementById("trefle5feuilles").innerHTML = `= ${res}`;
+    document.getElementById("cendres_eternelles").innerHTML = `= ${res}`;
+  }
+}
+
 // let affiche = false; // variable pour indiquer si l'input est affiché
 
 // function afficher() {
